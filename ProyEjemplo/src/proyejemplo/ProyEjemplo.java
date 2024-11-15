e36481b1a5022fb32fc77037591bd63af8160e3c
José Garces <joseggarces@protonmail.com>
2024-11-15 10:59:44 -0300

    Genere la clase Perro
    agregue un sout en el main saludando y luego cree la clase perro pero esta vacia.

--- ProyEjemplo/src/proyejemplo/ProyEjemplo.java
+++ ProyEjemplo/src/proyejemplo/ProyEjemplo.java
@@ -15,6 +15,11 @@ public class ProyEjemplo {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        
+        
+        
+        System.out.println("Hola Mundo");
+        
     }
     
 }
